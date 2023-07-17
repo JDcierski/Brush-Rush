a7d9ca23e6761f2592192bc89df0d5097d46273a
JDcierski <johnciecierski@comcast.net>
2023-07-17 16:19:07 -0400

    Now able to beat up child with toothbrush

---
+++ Assets/Scripts/Specific Use/ToothBrushInjury.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToothBrushInjury : MonoBehaviour
+{
+    public GameObject injury;
+    
+    public string zoneTag;
+    public bool inZone;
+    public float time;
+    private float lastTime;
+    public float timeInZone;
+
+    Vector3 pos, velocity;
+    public float targetVelocity;
+    public float xVelocity;
+    public float yVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        pos = transform.position;
+        inZone = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = (transform.position - pos) / Time.deltaTime;
+        pos = transform.position;
+        xVelocity = Mathf.Abs(velocity.x) / 100;
+        yVelocity = Mathf.Abs(velocity.y) / 100;
+
+        if(inZone && (velocity.x >= targetVelocity || velocity.y >= targetVelocity)){
+            time += Time.time - lastTime;
+        }
+        lastTime = Time.time;
+        if(time >= timeInZone){
+            Instantiate(injury, transform.position, transform.rotation);
+            time = 0;
+        }
+
+    }
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.tag == zoneTag){
+            inZone = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col){
+        if(col.gameObject.tag == zoneTag){
+            inZone = false;
+        }
+    }
+}
